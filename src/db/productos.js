5513e1605d92ce1b02a3a427bd9b6ec21b74dc78
unknown <jorge.aldeleon@gmail.com>
2025-02-12 01:03:36 -0300

    se insertaron las cartas de manera dinamica

---
+++ src/db/productos.js
@@ -0,0 +1,41 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Camiseta Titular Argentina 2024",
+        foto: "imgs/camiseta-argentina-2024.webp",
+        descripcion: "iphone 11 super pro max", 
+        precio: 222.20
+    },
+    {
+        id: 2,
+        nombre: "Camiseta Suplente Argentina 2024",
+        foto: "imgs/Camiseta-argentina-suplente-2024.webp",
+        descripcion: "Camiseta Suplente", 
+        precio: 333.30
+    },
+    {
+        id: 3,
+        nombre: "Camara-cannon",
+        foto: "imgs/camara-canon.jpg",
+        descripcion: "Camara ultra 4k", 
+        precio: 440.40
+    },
+    {
+        id: 4,
+        nombre: "Drone DJI",
+        foto: "imgs/drone-dji.jpg",
+        descripcion: "Altro drone", 
+        precio: 550.50
+    },
+    {
+        id: 5,
+        nombre: "Auriculares Sony",
+        foto: "imgs/auriculares-sony.jpg",
+        descripcion: "Cancelacion activa", 
+        precio: 660.60
+    }
+
+]
+
+
+export default productos
--- src/main.js
+++ src/main.js
@@ -1,1 +1,44 @@
+import productos from './db/productos'
 import './sass/main.scss'
+
+console.log(productos)
+
+const contenedorProductos = document.getElementById('container-productos')
+console.log(contenedorProductos)
+
+const start = () => {
+    console.warn('se cargo todo el html')
+
+
+    let html = ''
+
+    productos.forEach((prod) => {
+        console.log(prod)
+        html += `<div class="card">
+      <article class="card__article">
+      <div class="card__image-container">
+        <img class="card__image" src="${prod.foto}" alt="${prod.nombre}">
+      </div>
+      <div class="card__content">
+        <h2 class="card__heading">${prod.nombre}</h2>
+        <div class="card__description">
+          <p>${prod.descripcion}</p>
+        </div>
+      </div>        
+    </article>
+    </div>`
+    })
+
+    contenedorProductos.innerHTML = html
+}
+
+
+
+
+
+
+
+window.addEventListener('DOMContentLoaded', start)
+
+
+
